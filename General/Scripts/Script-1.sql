5da7046bb8b1251b90b19f0a256174b4c5487246
Gijs-Hoeben <gijs.hoeben@outlook.com>
2019-11-29 11:58:05 +0100

    script aangepast

--- General/Scripts/Script-1.sql
+++ General/Scripts/Script-1.sql
@@ -1,10 +1,29 @@
 
+drop table Navigationsystem;
+
 CREATE TABLE Navigationsystem (
 	ID INT PRIMARY KEY,
 	Satnavsystem Text NOT NULL,
 	CountryOfOrigin INT NOT NULL,
 	NumberOfSatellites INT NOT NULL,
-	since DATE NOT NULL,
-	Status text NOT NULL
+	Since DATE NOT NULL,
+	Status text NOT NULL,
+	FOREIGN KEY (CountryOfOrigin) REFERENCES country(ID)
+);
+CREATE TABLE country (
+	ID int PRIMARY KEY,
+	countryname text
+	
 );
 
+insert into country VALUES (0, 'united states');
+
+insert into country VALUES (1, 'Rusland');
+
+insert into country VALUES (2, 'China');
+
+insert into Navigationsystem VALUES (0, 'gps', 0, 32, 1967, 'Operationeel' );
+
+insert into Navigationsystem VALUES (1, 'glonass', 1, 24, 1982, 'Operationeel' );
+
+INSERT into Navigationsystem VALUES (2, 'Beidou', 2, 33, )
